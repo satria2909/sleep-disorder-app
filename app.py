d540dad6749daad789c75b5e866c20cb93ac76fd
Satria Dava Riansa <153600852+satria2909@users.noreply.github.com>
2026-07-09 02:07:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -123,25 +123,25 @@ elif menu == "🧮 Prediksi Tidur":
             st.warning("Nama wajib diisi.")
         else:
             response = requests.post(
-    	    	"http://sleep-disorder-app-production.up.railway.app/predict",
-       		    json={
-              	    "Age": umur,
-        	        "Sleep_Duration": durasi_tidur,
-        	        "Quality_of_Sleep": kualitas_tidur,
-        	        "Physical_Activity_Level": aktivitas_fisik,
-        	        "Stress_Level": tingkat_stres,
-        	        "Heart_Rate": heart_rate,
-        	        "Daily_Steps": daily_steps,
-        	        "Systolic_BP": systolic,
-        	        "Diastolic_BP": diastolic
-    	  	    }
-	        )
-
-		    hasil_api = response.json()
-	
-		    hasil = hasil_api["prediction"]
-	
-		    prob_dict = hasil_api["probability"]
+                "http://sleep-disorder-app-production.up.railway.app/predict",
+                json={
+                    "Age": umur,
+                    "Sleep_Duration": durasi_tidur,
+                    "Quality_of_Sleep": kualitas_tidur,
+                    "Physical_Activity_Level": aktivitas_fisik,
+                    "Stress_Level": tingkat_stres,
+                    "Heart_Rate": heart_rate,
+                    "Daily_Steps": daily_steps,
+                    "Systolic_BP": systolic,
+                    "Diastolic_BP": diastolic
+                }
+            )
+            
+            hasil_api = response.json()
+            
+            hasil = hasil_api["prediction"]
+            
+            prob_dict = hasil_api["probability"]
 
             st.session_state.last_pred = {
                 "BMI": bmi,
