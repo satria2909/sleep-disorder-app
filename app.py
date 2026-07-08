279016c84da06a15145cba8542e1213f763cdb84
Satria Dava Riansa <153600852+satria2909@users.noreply.github.com>
2026-07-09 01:47:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ elif menu == "🧮 Prediksi Tidur":
             st.warning("Nama wajib diisi.")
         else:
             response = requests.post(
-    	    	"http://127.0.0.1:8000/predict",
+    	    	"http://sleep-disorder-app-production.up.railway.app/predict",
        		json={
               	    "Age": umur,
         	    "Sleep_Duration": durasi_tidur,
